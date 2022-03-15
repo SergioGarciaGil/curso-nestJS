b15e1b01599eda58e4b3238debd1aa473563dcf9
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-03-15 09:54:24 -0500

    nest and

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true, //no deja agregar otra linea del producto de las ya establecidas
+    forbidNonWhitelisted:true //igual que whitelist pero le genera un mensaje de error
+   
+  }));
   await app.listen(3000);
 }
 
